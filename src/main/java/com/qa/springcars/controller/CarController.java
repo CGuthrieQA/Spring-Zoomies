69b4f117888244978211f2ea17c565179334a9a6
CGuthrieQA <CGuthrie@QA.com>
2021-07-07 13:39:50 +0100

    CG: spring day 3 lunch

--- src/main/java/com/qa/springcars/controller/CarController.java
+++ src/main/java/com/qa/springcars/controller/CarController.java
@@ -3,17 +3,21 @@ package com.qa.springcars.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.springcars.domain.Car;
 
 @RestController
+@RequestMapping("/cars")
 public class CarController {
 	
 	// Mapping URLs to METHODS
@@ -24,28 +28,29 @@ public class CarController {
 	
 	// CREATE
 	@PostMapping("/create")
-	public String carCreate(@RequestBody Car c) {
+	public ResponseEntity<String> carCreate(@RequestBody Car c) {
 		this.vehicles.add(c);
-		return "Successfully added car";
+		return new ResponseEntity<>("Successfully added car", HttpStatus.CREATED);
+		//return ResponseEntity.created(URI);
 	}
 	
 	// READ
 	
-		// All
+	// All
 	@GetMapping("/read")
-	public List<Car> carReadAll(){
-		return this.vehicles;
+	public ResponseEntity<List<Car>> carReadAll(){
+		return ResponseEntity.ok(this.vehicles);
 	}
 	
-	// ById
+	// ByIndex
 	@GetMapping("/read/{index}")
-	public Car carReadByIndex(@PathVariable int index) {
-		return this.vehicles.get(index);
+	public ResponseEntity<Car> carReadByIndex(@PathVariable int index) {
+		return ResponseEntity.ok(this.vehicles.get(index));
 	}
 	
 	// UPDATE - PUT (REPLACE) / PATCH (UPDATE an ELEMENT)
 	@PatchMapping("/update/{index}")
-	public Car carUpdate(@PathVariable int index, @RequestBody Car newCar) {
+	public ResponseEntity<Car> carUpdate(@PathVariable int index, @RequestBody Car newCar) {
 		
 //		Car toUpdate = this.vehicles.get(index);
 //		toUpdate.setMake(newCar.getMake());
@@ -55,15 +60,19 @@ public class CarController {
 //		return toUpdate;
 		
 		this.vehicles.set(index, newCar);
-		return this.vehicles.get(index);
+		return new ResponseEntity<>(this.vehicles.get(index), HttpStatus.ACCEPTED);
 		
 	}
 	
 	// DELETE - By Index
 	@DeleteMapping("/delete/{index}")
-	public String carDelete(@PathVariable int index) {
-		this.vehicles.remove(index);
-		return "Successfully deleted car";
+	public ResponseEntity<Car> carDelete(@PathVariable int index) {
+		
+//		return new ResponseEntity<>(this.vehicles.remove(index), HttpStatus.NO_CONTENT);
+		
+		return this.vehicles.remove(index) != null ?
+				new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 }
