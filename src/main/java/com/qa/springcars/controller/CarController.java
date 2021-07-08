9657f783899921db31c28877a3957de86e172440
CGuthrieQA <CGuthrie@QA.com>
2021-07-08 09:50:08 +0100

    CG: spring morning day 4

--- src/main/java/com/qa/springcars/controller/CarController.java
+++ src/main/java/com/qa/springcars/controller/CarController.java
@@ -20,44 +20,42 @@ import com.qa.springcars.service.CarServiceList;
 @RestController
 @RequestMapping("/cars") // http://127.0.0.1:8080/cars...
 public class CarController {
-	
+
 	// Mapping URLs to METHODS
-	
+
 	private CarServiceList service;
-	
+
 	// pull down object from the beanbag
 	@Autowired
 	public CarController(CarServiceList service) {
 		this.service = service;
 	}
-	
+
 	// CRUD Functionality
-	
+
 	// CREATE
 	@PostMapping("/create")
 	public ResponseEntity<String> carCreate(@RequestBody Car c) {
 		return new ResponseEntity<>(this.service.carCreate(c), HttpStatus.CREATED);
 	}
-	
+
 	// READ
 	@GetMapping("/read")
-	public ResponseEntity<List<Car>> carReadAll(){
-		return ResponseEntity.ok(this.service.carReadAll());	
+	public ResponseEntity<List<Car>> carReadAll() {
+		return ResponseEntity.ok(this.service.carReadAll());
 	}
-	
+
 	// UPDATE - PUT (REPLACE) / PATCH (UPDATE an ELEMENT)
 	@PatchMapping("/update/{index}")
 	public ResponseEntity<Car> carUpdate(@PathVariable int index, @RequestBody Car newCar) {
-		this.service.carUpdate(index, newCar);
-		return new ResponseEntity<>(newCar, HttpStatus.ACCEPTED);
+		return new ResponseEntity<>(this.service.carUpdate(index, newCar), HttpStatus.ACCEPTED);
 	}
-	
+
 	// DELETE - By Index
 	@DeleteMapping("/delete/{index}")
 	public ResponseEntity<Car> carDelete(@PathVariable int index) {
-		return this.service.carDelete(index) ?
-			new ResponseEntity<>(HttpStatus.NO_CONTENT)
-			: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		return this.service.carDelete(index) ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
+
 }
