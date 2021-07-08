63e5e5dac3a55a0ffbfd96dab621956d9b88251c
CGuthrieQA <CGuthrie@QA.com>
2021-07-08 21:30:58 +0100

    CG: added constructor to Boat class

--- src/main/java/com/qa/springcars/domain/Boat.java
+++ src/main/java/com/qa/springcars/domain/Boat.java
@@ -2,8 +2,14 @@ package com.qa.springcars.domain;
 
 public class Boat {
 	
-	//vars
+	// variables
 	private boolean floats;
+
+	// constructor
+	public Boat(boolean floats) {
+		super();
+		this.floats = floats;
+	}
 	
 	
 
